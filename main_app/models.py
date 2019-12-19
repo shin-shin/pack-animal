78824395f6ae4a9f76efc654c6250629504c1bcf
Irene Tokareva <longhorn09@gmail.com>
2019-12-18 22:23:49 -0600

    add destination model, basic auth, static JS files

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.urls import reverse
+from datetime import date
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Destination(models.Model):
+    location = models.CharField(max_length=100)
+    dates=models.DateField()
+    completed=models.IntegerField("completed", default=0)
