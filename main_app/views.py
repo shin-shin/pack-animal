c052e05fe856b7d2717da33db5c97c1550ef7eb7
Irene Tokareva <longhorn09@gmail.com>
2019-12-18 20:07:14 -0600

    add base template, home and about templates, urls and view

--- main_app/views.py
+++ main_app/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('<h1>Hello World!</h1>')
+    return render(request, "home.html")
+
+def about(request):
+    return render(request, "about.html")
