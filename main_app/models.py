c739d77ba82aae77c054cc316f8445f1da60ea44
Irene Tokareva <longhorn09@gmail.com>
2019-12-22 15:33:08 -0600

    update models

--- main_app/models.py
+++ main_app/models.py
@@ -6,5 +6,37 @@ from django.contrib.auth.models import User
 
 class Destination(models.Model):
     location = models.CharField(max_length=100)
-    dates=models.DateField()
-    completed=models.IntegerField("completed", default=0)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    completed = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f'trip to {self.location} from {self.start_date} to {self.end_date}'
+    
+    class Meta:
+        ordering = ['-start_date']
+
+class Day(models.Model):
+    date = models.DateField()
+    destination = models.ForeignKey(Destination, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f'{self.date} at {self.destination}'
+
+class Activity(models.Model):
+    name = models.CharField(max_length=200)
+    day = models.ForeignKey(Day, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name} on {self.day}'
+
+class Item(models.Model):
+    name = models.CharField(max_length=200)
+    is_checked = models.BooleanField(default=False)
+    destination = models.ForeignKey(Destination, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name} for a trip to {self.destination}'
+
+   
