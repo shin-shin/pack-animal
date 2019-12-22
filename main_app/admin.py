bd5e0017f1cc42e0cbde1be7aabbc403241d8dba
Irene Tokareva <longhorn09@gmail.com>
2019-12-22 15:49:27 -0600

    register new models in admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Destination
+from .models import Destination, Day, Activity, Item
 
 # Register your models here.
 admin.site.register(Destination)
+admin.site.register(Day)
+admin.site.register(Activity)
+admin.site.register(Item)
