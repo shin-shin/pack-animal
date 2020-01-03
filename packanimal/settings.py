53d36a0272063036ecda8ea5f0b61a6aa4ea3f1b
Matthew Sartain <matthewsartain@me.com>
2020-01-02 20:29:59 -0600

    another key test

--- packanimal/settings.py
+++ packanimal/settings.py
@@ -10,13 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 import os
+
 import environ
-env = environ.Env()
+environ.Env()
 environ.Env.read_env()
 
 
-GOOGLE_API_KEY =  env('GOOGLE_PLACES')
-DARKSKY_SECRET = env('DARKSKY_SECRET')
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
